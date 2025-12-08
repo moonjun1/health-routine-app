fe9796d2bdb55c1fc863ea40b538e28e4607eafe
moonjun1 <moonjun1@users.noreply.github.com>
2025-12-08 19:13:15 +0900

    chore: add Gradle build configuration files

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven { url = uri("https://repository.map.kakao.com/nexus/content/groups/public/") }
+    }
+}
+
+rootProject.name = "GymRoutine"
+include(":app")
+ 
