5f99c42d8514659430743110194cb6b1b3a578bf
moonjun1 <moonjun1@users.noreply.github.com>
2025-12-12 12:40:54 +0900

    refactor: 사용하지 않는 코드 제거
    - KakaoMapView.kt 삭제 (GoogleMapView 사용 중)
    - GymRoutineApp.kt 주석 처리된 카카오맵 초기화 코드 제거
    - 사용하지 않는 import 정리

--- app/src/main/java/com/example/gymroutine/GymRoutineApp.kt
+++ app/src/main/java/com/example/gymroutine/GymRoutineApp.kt
@@ -1,19 +1,8 @@
 package com.example.gymroutine
 
 import android.app.Application
-import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 // Hilt 의존성 주입을 위한 Application 클래스
 @HiltAndroidApp
-class GymRoutineApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        // 카카오맵 SDK 초기화
-        // NOTE: x86_64 에뮬레이터에서는 크래시 발생 (libK3fAndroid.so 미지원)
-        // 실제 기기나 ARM 에뮬레이터에서만 활성화하세요
-        // KakaoMapSdk.init(this, BuildConfig.KAKAO_REST_API_KEY)
-    }
-}
-
+class GymRoutineApp : Application()
